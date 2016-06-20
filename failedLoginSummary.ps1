ce4c6a6798986fabcdea35961a054dc8192b9ba7
Alex Turner <turneral1@grinnell.edu>
2016-06-20 08:41:33 -0500

    Initialized
    Created a script that prints failed login attempts made within the last
    week to an HTML file in Documents.

---
+++ failedLoginSummary.ps1
@@ -0,0 +1,41 @@
+﻿# Create a summary of failed logins to the user account during the last week.
+
+$Date= Get-date     
+
+$DC= "DESKTOP-SRQOBC2" #"TSDC1.HQ.TSTS.com"
+
+$filePath = "~/Documents/failedLogins/"
+if(!(Test-Path $filePath)) {
+   md $filePath
+}
+
+$Report= $filePath + $(get-date -f MM-dd-yyyy) + ".html"
+
+$HTML=@"
+<title>Event Logs Report</title>
+<style>
+BODY{background-color :#FFFFF}
+TABLE{Border-width:thin;border-style: solid;border-color:Black;border-collapse: collapse;}
+TH{border-width: 1px;padding: 1px;border-style: solid;border-color: black;background-color: ThreeDShadow}
+TD{border-width: 1px;padding: 2px;border-style: solid;border-color: black;background-color: Transparent}
+</style>
+"@
+
+$eventsDC= Get-Eventlog security -Computer $DC -InstanceId 4625 -After (Get-Date).AddDays(-7) |
+   Select TimeGenerated,ReplacementStrings |
+   % {
+     New-Object PSObject -Property @{
+      Source_Computer = $_.ReplacementStrings[13] 
+      UserName = $_.ReplacementStrings[5]
+      IP_Address = $_.ReplacementStrings[19]
+      Date = $_.TimeGenerated
+    }
+   }
+   
+$dailySummary = Get-Eventlog security -Computer $DC -InstanceId 4625 -After (Get-Date).AddDays(-7) | Group-Object {$_.TimeWritten.Date} | ConvertTo-Html -Property Count,Name
+
+
+
+$body = "<H2>The week of " + (Get-Date).AddDays(-7) + "</H2>" + "<p>" + $dailySummary + "</p>"
+$eventsDC | ConvertTo-Html -Property Source_Computer,UserName,IP_Address,Date -head $HTML -body $body |
+       Out-File $Report 
